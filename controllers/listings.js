04f4e2fad88209197abba044bcef134efda4d03f
Kanak Maurya <kanakmaurya22@gmail.com>
2026-05-08 12:18:59 +0530

    implement MVC,starability and Route Optimization

---
+++ controllers/listings.js
@@ -0,0 +1,70 @@
+const Listing = require("../models/listing.js");
+
+module.exports.index =
+    async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("./listings/index.ejs", { allListings });
+}
+
+module.exports.renderNewForm = (req, res) => {
+    res.render("./listings/new.ejs");
+}
+
+module.exports.showListing = async (req, res) => {
+    let { id } = req.params;
+    // id = id.trim();
+    const listing = await Listing.findById(id).populate({ path: "reviews", populate: { path: "author" } }).populate("owner");
+    if (!listing) {
+        req.flash("error", "Listing does not exist!");
+        return res.redirect("/listings");
+    }
+    res.render("./listings/show.ejs", { listing })
+}
+
+module.exports.createListing = async (req, res) => {
+    const newListing = new Listing(req.body.listing);
+    // Manually fix the image format before saving
+    newListing.image = { url: req.body.listing.image, filename: "listingimage" };
+    // to automatically add username to new listing created
+    newListing.owner = req.user._id;
+    await newListing.save();
+    req.flash("success", "New Listing Created");
+    res.redirect("/listings");
+}
+
+module.exports.renderEditForm = async (req, res) => {
+    let { id } = req.params;
+    id = id.trim();
+    const listing = await Listing.findById(id);
+    if (!listing) {
+        req.flash("error", "Listing does not exist!");
+        return res.redirect("/listings");
+    }
+
+    res.render("./listings/edit.ejs", { listing })
+}
+
+module.exports.updateListing = async (req, res) => {
+    let { id } = req.params;
+
+    let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+
+    if (typeof req.body.listing.image !== "undefined") {
+        let url = req.body.listing.image;
+        let filename = "listingimage";
+        listing.image = { url, filename };
+        await listing.save();
+    }
+    req.flash("success", "Listing Updated");
+
+
+    res.redirect(`/listings/${id}`);
+}
+
+module.exports.destroyListing = async (req, res) => {
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    req.flash("success", "Listing Deleted");
+    res.redirect("/listings");
+}
--- routes/listing.js
+++ routes/listing.js
@@ -6,85 +6,24 @@ const { listingSchema } = require("../schema.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 
+const listingController = require("../controllers/listings.js");
 
-
-//INDEX ROUTE
-router.get("/", wrapAsync(async (req, res) => {
-    const allListings = await Listing.find({});
-    res.render("./listings/index.ejs", { allListings });
-}))
+//INDEX AND CREATE ROUTE
+router.route("/")
+.get( wrapAsync(listingController.index))
+.post( validateListing, isLoggedIn, wrapAsync(listingController.createListing));
 
 //NEW ROUTE
-router.get("/new", isLoggedIn, (req, res) => {
-    res.render("./listings/new.ejs");
-})
+router.get("/new", isLoggedIn, listingController.renderNewForm);
 
-//SHOW ROUTE 
-router.get("/:id", wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    // id = id.trim();
-    const listing = await Listing.findById(id).populate({ path: "reviews", populate: { path: "author" } }).populate("owner");
-    if (!listing) {
-        req.flash("error", "Listing does not exist!");
-        return res.redirect("/listings");
-    }
-    res.render("./listings/show.ejs", { listing })
-}))
-
-//CREATE ROUTE
-router.post("/", validateListing, isLoggedIn, wrapAsync(async (req, res) => {
-    const newListing = new Listing(req.body.listing);
-    // Manually fix the image format before saving
-    newListing.image = { url: req.body.listing.image, filename: "listingimage" };
-    // to automatically add username to new listing created
-    newListing.owner = req.user._id;
-    await newListing.save();
-    req.flash("success", "New Listing Created");
-    res.redirect("/listings");
-}))
-
-
+//SHOW, UPDATE AND DELETE ROUTE
+router.route("/:id")
+.get( wrapAsync(listingController.showListing))
+.put( isLoggedIn, isOwner, validateListing, wrapAsync(listingController.updateListing))
+.delete( isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
 
 //EDIT ROUTE
-router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    id = id.trim();
-    const listing = await Listing.findById(id);
-    if (!listing) {
-        req.flash("error", "Listing does not exist!");
-        return res.redirect("/listings");
-    }
-
-    res.render("./listings/edit.ejs", { listing })
-}));
-
-//UPDATE ROUTE
-
-router.put("/:id", isLoggedIn, isOwner, validateListing, wrapAsync(async (req, res) => {
-    let { id } = req.params;
-
-    let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-
-    if (typeof req.body.listing.image !== "undefined") {
-        let url = req.body.listing.image;
-        let filename = "listingimage";
-        listing.image = { url, filename };
-        await listing.save();
-    }
-    req.flash("success", "Listing Updated");
-
-
-    res.redirect(`/listings/${id}`);
-}));
-
-//DELETE ROUTE
-router.delete("/:id", isLoggedIn, isOwner, wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    req.flash("success", "Listing Deleted");
-    res.redirect("/listings");
-}));
+router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.renderEditForm));
 
 module.exports = router;
 
